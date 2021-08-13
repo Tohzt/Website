bf8bdfb49edacff17da530c954da7536c98ffc57
Tohzt <Steven.G.French@live.com>
2021-08-12 21:45:04 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+			</header>
+			<body>
+				<h2>Testington...</h2>
+			</body>
+		</div>
+	);
+}
+
+export default App;
